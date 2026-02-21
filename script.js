0836963d855df98408cd355308a4a682e304f42b
Jason_fw <jason.ffweic@gmail.com>
2026-02-21 15:18:39 +0100

    Add fetch function to load and display Einsatz data

---
+++ script.js
@@ -0,0 +1,22 @@
+fetch("einsatz/einsaetze.json")
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById("einsatzListe");
+
+    data.sort((a, b) => {
+        const dateA = new Date(a.datum.split(".").reverse().join("-"));
+        const dateB = new Date(b.datum.split(".").reverse().join("-"));
+        return dateB - dateA;
+    });
+
+    data.forEach(einsatz => {
+      container.innerHTML += `
+        <div class="card">
+          <h3>🔥 ${einsatz.titel}</h3>
+          <p><strong>Datum:</strong> ${einsatz.datum}</p>
+          <p><strong>Stichwort:</strong> ${einsatz.stichwort}</p>
+          <a class="button" href="${einsatz.link}">Zum Einsatzbericht</a>
+        </div>
+      `;
+    });
+  });
