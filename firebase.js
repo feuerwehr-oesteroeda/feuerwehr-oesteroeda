e52bd53c05cf8dea462ae7bf1619664a6fb3adba
Jason_fw <jason.ffweic@gmail.com>
2026-02-22 20:36:52 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCGGp2iELnHCQdzV93OR7FrdQqf_-EJu4s",
+  authDomain: "feuerwehr-oesteroeda-1d68e.firebaseapp.com",
+  projectId: "feuerwehr-oesteroeda-1d68e",
+  storageBucket: "feuerwehr-oesteroeda-1d68e.firebasestorage.app",
+  messagingSenderId: "661355746233",
+  appId: "1:661355746233:web:246f561d52c5c1f1585c68"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
